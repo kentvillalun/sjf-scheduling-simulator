b435a546d97e98de8859a748343ab83dcce55a9b
KentDoCode <villalunkent03@gmail.com>
2026-01-24 02:03:32 +0800

    Made a reveal on screen effect and finish process setup input

--- src/components/sections/Form.jsx
+++ src/components/sections/Form.jsx
@@ -1,13 +1,37 @@
-import { useState } from "react"
-
+import { useState } from "react";
+import { RevealOnScroll } from "../RevealOnScroll";
 
 export const Form = () => {
+  const [step, setStep] = useState(1);
 
-    const [step, setStep] = useState(1);
+  return (
+    <section className="flex items-center justify-center  py-10">
+      {step === 1 && (
+        <RevealOnScroll>
+          <div className="mx-auto px-6 max-w-3xl md:max-w-5xl flex flex-col items-center justify-center gap-4">
+            <h2 className="text-2xl font-medium">Process Setup</h2>
 
-    return (
-        <section className="flex flex-col justify-center items-center m-auto text-center p-6 gap-2">
-            {}
-        </section>
-    )
-}
+            <form
+              action=""
+              className="w-full border m-auto p-10 border-white/10 rounded-3xl flex flex-col gap-6 bg-white/5"
+            >
+              <p className="text-gray-300 text-center">
+                How many processes do you want to input?
+              </p>
+
+              <div className="flex flex-col md:flex-row gap-4">
+                <input
+                  type="number"
+                  className="focus:outline-none px-4 py-2 bg-white/8 rounded-lg border-white/5 border "
+                />
+                <button className="bg-black px-4 py-3 rounded-lg cursor-pointer font-semibold text-sm hover:-translate-y-0.5 transition-all duration-200 shadow-[0_0_24px_rgba(34,42,53,0.06),0_1px_1px_rgba(0,0,0,0.05),0_0_0_1px_rgba(34,42,53,0.04),0_0_4px_rgba(34,42,53,0.08),0_16px_68px_rgba(47,48,55,0.05),0_1px_0_rgba(255,255,255,0.1)_inset] ">
+                  Generate Table
+                </button>
+              </div>
+            </form>
+          </div>
+        </RevealOnScroll>
+      )}
+    </section>
+  );
+};
--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -1,11 +1,17 @@
-
+import { RevealOnScroll } from "../RevealOnScroll";
 
 export const Header = () => {
-    return (
-        <section className="flex flex-col justify-center items-center text-center p-6 gap-2 mt-30">
-            
-            <h1 className="text-3xl md:text-5xl font-bold">Shortest Job First Algorithm</h1>
-            <p className="text-gray-300 md:text-xl">A CPU scheduling algorithm that selects the process with the shortest Burst Time (BT) from the ready queue.</p>
-        </section>
-    )
-}
+  return (
+    <section className="flex flex-col justify-start items-center text-center p-6 gap-2 mt-30 max-w-5xl">
+      <RevealOnScroll>
+        <h1 className="text-3xl md:text-5xl font-bold">
+          Shortest Job First Algorithm
+        </h1>
+        <p className="text-gray-300 md:text-xl">
+          A CPU scheduling algorithm that selects the process with the shortest
+          Burst Time (BT) from the ready queue.
+        </p>
+      </RevealOnScroll>
+    </section>
+  );
+};
