889343894da628df0f29298e2a7a56ccd2a45a1f
KentDoCode <villalunkent03@gmail.com>
2026-01-24 19:00:19 +0800

    Made: table

--- src/components/sections/Form.jsx
+++ src/components/sections/Form.jsx
@@ -4,6 +4,12 @@ import { RevealOnScroll } from "../RevealOnScroll";
 export const Form = () => {
   const [step, setStep] = useState(1);
 
+  const [processCount, setProcessCount] = useState(0);
+
+  const handleGenerate = (event) => {
+    setProcessCount(Number(event.target.value));
+  };
+
   return (
     <RevealOnScroll>
       <section className="flex items-center justify-center  py-10">
@@ -24,8 +30,10 @@ export const Form = () => {
                   type="number"
                   className="focus:outline-none px-4 py-2 bg-white/8 rounded-lg border-white/5 border focus:border-white/20 transition-all duration-200"
                   placeholder="e.g. 5"
+                  onChange={handleGenerate}
                 />
                 <button
+                  type="button"
                   className="bg-black px-4 py-3 rounded-lg cursor-pointer font-semibold text-sm hover:-translate-y-0.5 transition-all duration-200 shadow-[0_0_24px_rgba(34,42,53,0.06),0_1px_1px_rgba(0,0,0,0.05),0_0_0_1px_rgba(34,42,53,0.04),0_0_4px_rgba(34,42,53,0.08),0_16px_68px_rgba(47,48,55,0.05),0_1px_0_rgba(255,255,255,0.1)_inset] "
                   onClick={() => {
                     setStep(step + 1);
@@ -39,17 +47,32 @@ export const Form = () => {
         )}
 
         {step === 2 && (
-          <div className="mx-auto px-6 max-w-3xl md:max-w-5xl flex flex-col items-center justify-center gap-4">
-            <h2 className="text-2xl font-medium">Process Details</h2>
-            <div className="text-gray-300 text-center space-y-0.5">
-              <p className="">Fill in the details for each process below.</p>
-              <p className="italic">
-                Note: Process names should be unique (e.g., A, B, C or P1, P2,
-                P3)
-              </p>
+          <div className="mx-auto px-6 max-w-3xl md:max-w-5xl flex flex-col items-center justify-center gap-7">
+            <div className="flex flex-col items-center justify-center gap-2">
+              <h2 className="text-2xl font-medium">Process Details</h2>
+              <div className="text-gray-300 text-center space-y-0.5">
+                <p className="">Fill in the details for each process below.</p>
+                <p className="italic">
+                  Note: Process names should be unique (e.g., A, B, C or P1, P2,
+                  P3)
+                </p>
+              </div>
             </div>
+            <div className="grid grid-cols-1 gap-6">
+              <div className="grid grid-cols-3 text-center gap-6 items-center justify-center font-medium md:gap-40 ">
+                <div className="">Process</div>
+                <div className="">Burst Time</div>
+                <div className="">Arrival Time</div>
+              </div>
 
-            
+              <div className="grid grid-cols-3 text-center gap-6 items-center justify-center font-medium md:gap-40">
+                <input type="text" className="focus:outline-none px-4 py-2 bg-white/8 rounded-lg border-white/5 border focus:border-white/20 transition-all duration-200 text-center" />
+                <input type="text" className="focus:outline-none px-4 py-2 bg-white/8 rounded-lg border-white/5 border focus:border-white/20 transition-all duration-200 text-center" />
+                <input type="text" className="focus:outline-none px-4 py-2 bg-white/8 rounded-lg border-white/5 border focus:border-white/20 transition-all duration-200 text-center" />
+                
+
+              </div>
+            </div>
           </div>
         )}
       </section>
